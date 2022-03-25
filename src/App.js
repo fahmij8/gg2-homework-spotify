e07ea7b5c52783a88030166923f8312dc91f0bba
Fahmi Jabbar <rootmawar77@gmail.com>
2022-03-25 22:37:18 +0700

    Finish Module 2 Session 3 Homework - Switch to tailwind via npm - Render song data to table

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import "./App.css";
-import playlistData from "./data";
+import playlistData from "data";
 import Track from "components/track";
 
 function App() {
     return (
-        <div className="App">
-            <h1 className="text-3xl text-center mt-3 mb-0 font-semibold text-gray-800">
+        <div className="bg-zinc-800 min-h-screen py-6">
+            <h1 className="text-3xl text-center mt-0 font-semibold text-white">
                 My Playlist
             </h1>
-            <p className="text-center font-light text-gray-800 mt-0 mb-3">
+            <p className="text-center font-light text-white mt-0 mb-3">
                 by Fahmi Jabbar (KM_G2FE4088)
             </p>
-            <div className="py-4">
-                <div className="container mx-auto px-4">
-                    <div className="flex justify-center">
-                        <Track songData={playlistData}></Track>
-                    </div>
-                </div>
-            </div>
+            <Track songData={playlistData}></Track>
         </div>
     );
 }
--- src/components/track/index.js
+++ src/components/track/index.js
@@ -1,35 +1,113 @@
+function millisToMinutesAndSeconds(millis) {
+    const minutes = Math.floor(millis / 60000);
+    const seconds = ((millis % 60000) / 1000).toFixed(0);
+    return minutes + ":" + (seconds < 10 ? "0" : "") + seconds;
+}
+
 function Track({ songData }) {
     return (
-        <div className="max-w-xs overflow-hidden rounded-lg shadow-lg hover:shadow-2xl">
-            <img
-                className="w-full"
-                src={songData.album.images[1].url}
-                alt={songData.album.name}
-            />
-            <div className="px-6 py-4">
-                <h4 className="text-xl font-semibold tracking-tight text-gray-800 hover:text-blue-500">
-                    <a
-                        href={songData.external_urls.spotify}
-                        target="_blank"
-                        rel="noreferrer"
-                    >
-                        {songData.name}
-                    </a>
-                </h4>
-                <p className="text-gray-700">
-                    by
-                    <a
-                        href={songData.artists[0].external_urls.spotify}
-                        target="_blank"
-                        rel="noreferrer"
-                        className="ml-1 hover:text-blue-500"
-                    >
-                        {songData.artists[0].name}
-                    </a>
-                </p>
-                <button className="mt-4 mr-0 ml-auto block h-10 px-5 text-indigo-100 transition-colors duration-150 bg-indigo-700 rounded-lg focus:shadow-outline hover:bg-indigo-800">
-                    Select
-                </button>
+        <div className="flex flex-col mx-6 sm:mx-11 md:mx-14">
+            <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
+                <div className="py-2 inline-block min-w-full sm:px-6 lg:px-8">
+                    <div className="overflow-hidden">
+                        <table className="min-w-full">
+                            <thead className="bg-transparent border-b border-white/20">
+                                <tr>
+                                    <th
+                                        scope="col"
+                                        className="text-sm font-medium text-white px-6 py-4 text-center w-[10px]"
+                                    >
+                                        #
+                                    </th>
+                                    <th
+                                        scope="col"
+                                        className="text-sm font-medium text-white px-6 py-4 text-left min-w-[200px]"
+                                    >
+                                        Title
+                                    </th>
+                                    <th
+                                        scope="col"
+                                        className="text-sm font-medium text-white px-6 py-4 text-left min-w-[200px]"
+                                    >
+                                        Album
+                                    </th>
+                                    <th
+                                        scope="col"
+                                        className="text-sm font-medium text-white px-6 py-4 text-center max-w-[5%]"
+                                    >
+                                        Duration
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {songData.map((song, index) => {
+                                    return (
+                                        <tr
+                                            className="bg-transparent transition duration-300 ease-in-out hover:bg-gray-100/10"
+                                            key={song.id}
+                                        >
+                                            <td className="px-6 py-4 whitespace-normal text-sm font-light text-white text-center">
+                                                {index + 1}
+                                            </td>
+                                            <td className="text-sm text-white font-light px-6 py-4 whitespace-normal">
+                                                <div className="flex flex-row items-center">
+                                                    <img
+                                                        src={
+                                                            song.album.images[0]
+                                                                .url
+                                                        }
+                                                        alt={song.album.name}
+                                                        className="w-11 h-11 mr-4"
+                                                    />
+                                                    <div className="flex flex-col">
+                                                        <span className="text-sm font-medium text-white">
+                                                            {song.name}
+                                                        </span>
+                                                        <div className="inline max-w-fit">
+                                                            <a
+                                                                href={
+                                                                    song
+                                                                        .artists[0]
+                                                                        .external_urls
+                                                                        .spotify
+                                                                }
+                                                                className="inline-block text-sm font-light text-white/80 hover:text-white hover:underline"
+                                                            >
+                                                                {
+                                                                    song
+                                                                        .artists[0]
+                                                                        .name
+                                                                }
+                                                            </a>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </td>
+                                            <td className="text-sm text-white font-light px-6 py-4 whitespace-normal">
+                                                <a
+                                                    className="inline-block text-sm font-light text-white/80 hover:text-white hover:underline"
+                                                    href={
+                                                        song.album.external_urls
+                                                            .spotify
+                                                    }
+                                                >
+                                                    {song.album.name}
+                                                </a>
+                                            </td>
+                                            <td className="text-sm text-white font-light px-6 py-4 whitespace-normal text-center">
+                                                <span className="text-white/80">
+                                                    {millisToMinutesAndSeconds(
+                                                        song.duration_ms
+                                                    )}
+                                                </span>
+                                            </td>
+                                        </tr>
+                                    );
+                                })}
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
             </div>
         </div>
     );
